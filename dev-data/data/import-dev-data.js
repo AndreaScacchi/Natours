76b583114ea726e109bcdd88d2d2e78c27a4bc3f
AndreaScacchi <andreascacchi10@gmail.com>
2021-11-29 16:36:34 +0100

    Deleted tours and added new tours from json

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -20,7 +20,7 @@ mongoose
     .then(() => console.log('DB connection successful!'));
 
 // READ JSON FILE
-const tours = JSON.parse(fs.readFileSync('tours-simple.json', 'utf-8'));
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'));
 
 // IMPORT DATA INTO DB
 const importData = async () => {
@@ -30,6 +30,7 @@ const importData = async () => {
     } catch(err) {
         console.log(err);
     }
+    process.exit();
 };
 
 
@@ -41,4 +42,12 @@ const deleteData = async () => {
         } catch (err) {
         console.log(err);
     }
+    process.exit();
 }
+
+if(process.argv[2] === '--import') {
+    importData();
+} else if(process.argv[2] === '--delete') {
+    deleteData();
+}
+
