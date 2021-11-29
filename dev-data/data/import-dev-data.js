939a7e5e3fc9f7e2086587ccaeaf4b16413486a2
AndreaScacchi <andreascacchi10@gmail.com>
2021-11-29 16:19:29 +0100

    Import data into db

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+const Tour = require('./../../models/tourModel');
+
+
+const DB = process.env.DATABASE.replace(
+    '<PASSWORD>',
+    process.env.DATABASE_PASSWORD
+);
+
+mongoose
+    .connect(DB, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log('DB connection successful!'));
+
+// READ JSON FILE
+const tours = JSON.parse(fs.readFileSync('tours-simple.json', 'utf-8'));
+
+// IMPORT DATA INTO DB
+const importData = async () => {
+    try {
+        await Tour.create(tours);
+        console.log('Data successfully loaded!');
+    } catch(err) {
+        console.log(err);
+    }
+};
